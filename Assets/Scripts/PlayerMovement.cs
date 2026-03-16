d0f478b15fcc5daa281028cdbaa65fd3406eadc6
Jules Goy <81039601+Witchoy@users.noreply.github.com>
2026-03-16 22:34:34 +0100

    feat: removed jumping functionnality for now

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -9,14 +9,11 @@ public class PlayerMovement : MonoBehaviour
 
     // Movement parameters
     public float moveSpeed = 5f;
-    public float jumpHeight = 0.5f;
 
     // Actions
     InputAction moveAction;
-    InputAction jumpAction;
 
     // State variables
-    private bool isJumpPressed;
 
     // Start is called before the first frame update
     void Start()
@@ -24,7 +21,6 @@ public class PlayerMovement : MonoBehaviour
         try
         {
             moveAction = InputSystem.actions.FindAction("Move");
-            jumpAction = InputSystem.actions.FindAction("Jump");
 
             rb = GetComponent<Rigidbody>();
         }
@@ -37,25 +33,9 @@ public class PlayerMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        isJumpPressed = jumpAction != null && jumpAction.triggered;
     }
 
     void FixedUpdate()
     {
-        if (isJumpPressed)
-        {
-            Jump();
-        }
-    }
-
-    void Jump()
-    {
-        if (rb == null)
-        {
-            Debug.LogError("Rigidbody component not found on player.");
-            return;
-        }
-        float jumpForce = rb.mass * Mathf.Sqrt(2f * 9.81f * jumpHeight);
-        rb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
     }
 }
